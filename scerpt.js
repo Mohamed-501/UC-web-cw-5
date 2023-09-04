9eedae0e857f48cea04ad4b9ce0236035d142c2c
Mohamed-501 <142637784+Mohamed-501@users.noreply.github.com>
2023-09-04 19:44:57 +0300

    I,m done

---
+++ scerpt.js
@@ -0,0 +1,25 @@
+alert ("مرحباً بك في موقعي! أنا محمد وسوف اقوم بحساب درجتك")
+
+let grad = prompt ("ادخل درجتك")
+console.log (grad)
+if (grad >= 90 ) { console.log( "لقد حصلت على امتياز 🥳");
+
+
+
+} else if (grad >= 80 ) {
+    console.log ( "لقد حصلت على جيد جداً🤩")
+
+}
+else if (grad >= 70 ) {console.log("لقد حصلت على جيد🙂")}
+
+else if (grad >= 60 ) {console.log("لقد حصلت على مقبول😕")}
+
+else if (grad >= 50 ) {console.log("لقد حصلت على ضعيف☹️")}
+
+else {console.log(راسب)}
+
+
+/*
+جافاسكربت ليست جافا
+
+*/
